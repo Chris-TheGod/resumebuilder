ef8201d036a959c91117c2a3dcf798dc20530184
Kristjan Lääne <xonproject@gmail.com>
2024-11-26 18:30:42 +0200

    resume editor start

---
+++ src/app/(main)/editor/ResumeEditor.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function ResumeEditor() {
+  return (
+    <div className="flex grow flex-col">
+      <header className="space-y-1.5 border-b px-3 py-5 text-center">
+        <h1 className="text-2xl font-bold">Design your resume</h1>
+        <p className=""></p>
+      </header>
+    </div>
+  );
+}
