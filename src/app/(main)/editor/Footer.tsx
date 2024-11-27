69e2c273fef8d0011ec1403d92c2feea6f3b2bcc
Kristjan Lääne <xonproject@gmail.com>
2024-11-27 19:08:21 +0200

    next and prev step buttons logic

--- src/app/(main)/editor/Footer.tsx
+++ src/app/(main)/editor/Footer.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { steps } from "./steps";
 
 type FooterProps = {
   currentStep: string;
@@ -7,12 +8,33 @@ type FooterProps = {
 };
 
 export default function Footer({ currentStep, setCurrentStep }: FooterProps) {
+  const previousStep = steps.find(
+    (_, index) => steps[index + 1]?.key === currentStep,
+  )?.key;
+
+  const nextStep = steps.find(
+    (_, index) => steps[index - 1]?.key === currentStep,
+  )?.key;
+
   return (
     <footer className="w-full border-t px-3 py-5">
       <div className="mx-auto flex max-w-7xl flex-wrap justify-between gap-3">
         <div className="flex items-center gap-3">
-          <Button variant="secondary">Previous step</Button>
-          <Button>Next step</Button>
+          <Button
+            variant="secondary"
+            onClick={
+              previousStep ? () => setCurrentStep(previousStep) : undefined
+            }
+            disabled={!previousStep}
+          >
+            Previous step
+          </Button>
+          <Button
+            onClick={nextStep ? () => setCurrentStep(nextStep) : undefined}
+            disabled={!nextStep}
+          >
+            Next step
+          </Button>
         </div>
         <div className="flex items-center gap-3">
           <Button variant="secondary" asChild>
