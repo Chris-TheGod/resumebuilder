c621730c20d6d6d282cc0469bdfb2f2243a79446
Kristjan Lääne <xonproject@gmail.com>
2024-11-27 18:14:47 +0200

    PersonalInfoForm start

--- src/app/(main)/editor/forms/PersonalInfoForm.tsx
+++ src/app/(main)/editor/forms/PersonalInfoForm.tsx
@@ -1,3 +1,59 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { personalInfoSchema, PersonalInfoValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 export default function PersonalInfoForm() {
-  return <div></div>;
+  const form = useForm<PersonalInfoValues>({
+    resolver: zodResolver(personalInfoSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      jobTitle: "",
+      city: "",
+      country: "",
+      phone: "",
+      email: "",
+    },
+  });
+
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">Personal info</h2>
+        <p className="text-sm text-muted-foreground">Tell us about yourself</p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          <FormField
+            control={form.control}
+            name="photo"
+            render={({ field: { value, ...fieldValues } }) => (
+              <FormItem>
+                <FormLabel>Your photo</FormLabel>
+                <FormControl>
+                  <Input
+                    {...fieldValues}
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => {
+                      const file = e.target.files?.[0];
+                      fieldValues.onChange(file);
+                    }}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 }
