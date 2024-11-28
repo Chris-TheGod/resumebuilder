b9cf4e1814a6eb0053a70b8468da25a5f2796d14
Kristjan Lääne <xonproject@gmail.com>
2024-11-28 18:49:59 +0200

    education form

--- src/app/(main)/editor/forms/EducationForm.tsx
+++ src/app/(main)/editor/forms/EducationForm.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import {
+  Form,
   FormControl,
   FormField,
   FormItem,
@@ -41,7 +42,43 @@ export default function EducationForm({
     name: "educations",
   });
 
-  return <div>EducationForm</div>;
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">Education</h2>
+        <p className="text-sm text-muted-foreground">
+          Add as many educations as you like.
+        </p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          {fields.map((field, index) => (
+            <EducationItem
+              key={field.id}
+              index={index}
+              form={form}
+              remove={remove}
+            />
+          ))}
+          <div className="flex justify-center">
+            <Button
+              type="button"
+              onClick={() =>
+                append({
+                  degree: "",
+                  school: "",
+                  startDate: "",
+                  endDate: "",
+                })
+              }
+            >
+              Add education
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 }
 
 type EducationItemProps = {
@@ -51,63 +88,73 @@ type EducationItemProps = {
 };
 
 function EducationItem({ form, index, remove }: EducationItemProps) {
-  <div className="space-y-3 rounded-md border bg-background p-3">
-    <div className="flex justify-between gap-2">
-      <span className="font-semibold">Education {index + 1}</span>
-      <GripHorizontal className="size-5 cursor-grab text-muted-foreground" />
-    </div>
-    <FormField
-      control={form.control}
-      name={`educations.${index}.degree`}
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>Degree</FormLabel>
-          <FormControl>
-            <Input {...field} autoFocus />
-          </FormControl>
-        </FormItem>
-      )}
-    />
-    <FormField
-      control={form.control}
-      name={`educations.${index}.school`}
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>School</FormLabel>
-          <FormControl>
-            <Input {...field} />
-          </FormControl>
-        </FormItem>
-      )}
-    />
-    <div className="grid grid-cols-2 gap-3">
+  return (
+    <div className="space-y-3 rounded-md border bg-background p-3">
+      <div className="flex justify-between gap-2">
+        <span className="font-semibold">Education {index + 1}</span>
+        <GripHorizontal className="size-5 cursor-grab text-muted-foreground" />
+      </div>
       <FormField
         control={form.control}
-        name={`educations.${index}.startDate`}
+        name={`educations.${index}.degree`}
         render={({ field }) => (
           <FormItem>
-            <FormLabel>Start date</FormLabel>
+            <FormLabel>Degree</FormLabel>
             <FormControl>
-              <Input {...field} type="date" value={field.value?.slice(0, 10)} />
+              <Input {...field} autoFocus />
             </FormControl>
           </FormItem>
         )}
       />
       <FormField
         control={form.control}
-        name={`educations.${index}.endDate`}
+        name={`educations.${index}.school`}
         render={({ field }) => (
           <FormItem>
-            <FormLabel>End date</FormLabel>
+            <FormLabel>School</FormLabel>
             <FormControl>
-              <Input {...field} type="date" value={field.value?.slice(0, 10)} />
+              <Input {...field} />
             </FormControl>
           </FormItem>
         )}
       />
+      <div className="grid grid-cols-2 gap-3">
+        <FormField
+          control={form.control}
+          name={`educations.${index}.startDate`}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Start date</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="date"
+                  value={field.value?.slice(0, 10)}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name={`educations.${index}.endDate`}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>End date</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="date"
+                  value={field.value?.slice(0, 10)}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </div>
+      <Button variant="destructive" type="button" onClick={() => remove(index)}>
+        Remove
+      </Button>
     </div>
-    <Button variant="destructive" type="button" onClick={() => remove(index)}>
-      Remove
-    </Button>
-  </div>;
+  );
 }
