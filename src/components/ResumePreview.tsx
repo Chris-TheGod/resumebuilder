47fe511640be919e0fc14c0c0b95c45216a4ca90
Kristjan Lääne <xonproject@gmail.com>
2024-11-30 14:02:32 +0200

    useDimensions implementation

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -1,5 +1,7 @@
+import useDimensions from "@/hooks/useDimensions";
 import { cn } from "@/lib/utils";
 import { ResumeValues } from "@/lib/validation";
+import { useRef } from "react";
 
 type ResumePreviewProps = {
   resumeData: ResumeValues;
@@ -10,16 +12,28 @@ export default function ResumePreview({
   resumeData,
   className,
 }: ResumePreviewProps) {
+  const cotainerRef = useRef<HTMLDivElement>(null);
+
+  const { width } = useDimensions(cotainerRef);
+
   return (
     <div
       className={cn(
         "aspect-[210/297] h-fit w-full bg-white text-black",
         className,
       )}
+      ref={cotainerRef}
     >
-      <h1 className="p-6 text-3xl font-bold">
-        This text should change with the size of the container div
-      </h1>
+      <div
+        className={cn("space-y-6 p-6", !width && "invisible")}
+        style={{
+          zoom: (1 / 794) * width,
+        }}
+      >
+        <h1 className="p-6 text-3xl font-bold">
+          This text should change with the size of the container div
+        </h1>
+      </div>
     </div>
   );
 }
