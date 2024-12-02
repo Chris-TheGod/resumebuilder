3d3c5b65cbd7c02323d358962afb83d53ba14567
Kristjan Lääne <xonproject@gmail.com>
2024-12-02 17:15:57 +0200

    saveResume server action implementation

--- src/app/(main)/editor/useAutoSaveResume.ts
+++ src/app/(main)/editor/useAutoSaveResume.ts
@@ -1,22 +1,52 @@
+import { useToast } from "@/hooks/use-toast";
 import useDebounce from "@/hooks/useDebounce";
 import { ResumeValues } from "@/lib/validation";
+import { set } from "date-fns";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { saveResume } from "./actions";
 
 export default function useAutoSaveResume(resumeData: ResumeValues) {
+  const searchParams = useSearchParams();
+
+  const { toast } = useToast();
+
   const debouncedResumeData = useDebounce(resumeData, 1500);
+
+  const [resumeId, setResumeId] = useState(resumeData.id);
 
   const [lastSavedData, setLastSavedData] = useState(
     structuredClone(resumeData),
   );
 
   const [isSaving, setIsSaving] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsError(false);
+  }, [debouncedResumeData]);
 
   useEffect(() => {
     async function save() {
-      setIsSaving(true);
-      await new Promise((resolve) => setTimeout(resolve, 1500));
-      setLastSavedData(structuredClone(debouncedResumeData));
-      setIsSaving(false);
+      try {
+        setIsSaving(true);
+        setIsError(false);
+
+        const newData = structuredClone(debouncedResumeData);
+
+        const updatedResume = await saveResume({
+          ...newData,
+          ...(lastSavedData.photo?.toString() === newData.photo?.toString() && {
+            photo: undefined,
+          }),
+          id: resumeId,
+        });
+
+        setResumeId(updatedResume.id);
+        setLastSavedData(newData);
+      } catch (error) {
+        console.log(error);
+      }
     }
 
     const hasUnsavedChanges =
